fe89fc78a644d211472bf54a47a7c0a2d0ef6c11
tom zhou <zs68j2ee@gmail.com>
2014-06-24 00:04:26 +0800

    added each-other socket.io ns

--- each-other/app.js
+++ each-other/app.js
@@ -1,1 +1,18 @@
-var running = deskShell.startApp({"user-data-dir": __dirname+"/data"});
+var running = deskShell.startApp({
+	"user-data-dir": __dirname+"/data",
+	"deskShellSocketClient": true
+});
+
+running.then(function(app){
+	console.log('app started ... done');
+								
+	// each-other namespace
+	app.socketio.of('/eachother').on('connection', function(socket){
+		socket.on('ping', function(data, fn){
+			console.log('data:'+data.data);
+			socket.emit('pong', {data: 'pong '+data.data});
+			if (fn) fn({data: 'pong '+data.data});
+		});	
+	});
+	
+});
