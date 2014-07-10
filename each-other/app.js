73ba381ea8b62b5ef91d7bdc93ef0cdea31acdce
tom zhou <zs68j2ee@gmail.com>
2014-07-10 23:31:06 +0800

    fixed http proxy server startup

--- each-other/app.js
+++ each-other/app.js
@@ -1,9 +1,11 @@
 var exec = require('child_process').exec,
     child;
+var http = require('http');
+var fs = require('fs');
 
 var forwardProxy = require('forward-proxy');
 var prxySrv = new forwardProxy({
-	    usrkey: 'helloword', 
+	    usrkey: 'hellword', 
 		secmode: 'acl', 
 		access_local: false
     }, function(err, proxy){
@@ -21,50 +23,64 @@ var prxySrv = new forwardProxy({
 	// get free tcp port
 	deskShell
 	.getFreePort(null, null)
-	.then(function(data){
+	.then(function(prxydata){
 		// 2.
 		// start http proxy service
-		var http = require('http');
 		var pxySrv = http.createServer();
-
+		
 		pxySrv.on('request', importApp.httpApp.proxy);
 		pxySrv.on('connect', importApp.httpApp.tunnel);
+			
+		pxySrv.listen(prxydata.port, 50);
+		console.log('Http forwar proxy server listen on port '+prxydata.port);
+		
+        // 3.
+        // start pac server
+        deskShell
+        .getFreePort(null, null)
+        .then(function(pacdata){
+            // pac server
+            var rawstr = fs.readFileSync(__dirname+'/auto.pac').toString('utf-8');
+        	// fill http proxy server
+        	var pacstr = rawstr.replace(/proxy_port/gi, ''+prxydata.port);
+            ///console.log('pacstr: '+pacstr);
+        	var pacsrv = http.createServer(function(req, res){
+        	    res.writeHead(200, {'Content-Type': 'application/x-ns-proxy-autoconfig'});
+        	    res.end(pacstr);
+        	});
 
-		pxySrv.listen(data.port, 50);
-		console.log('Http forwar proxy server listen on port '+data.port);
+        	pacsrv.listen(pacdata.port);
+        	console.log('pac server listening on '+pacdata.port);
+        	
+        	// 3.
+        	// start front app
+        	var running = deskShell.startApp({
+        		"user-data-dir": __dirname+"/data",
+        		"deskShellSocketClient": true
+        	});
 
-		// 3.
-		// start front app
-		var running = deskShell.startApp({
-			"user-data-dir": __dirname+"/data",
-			"deskShellSocketClient": true
-		});
+        	running.then(function(app){
+        		console.log('app started ... done');
 
-		running.then(function(app){
-			console.log('app started ... done');
+        		// 4.
+        		// launching chrome browser with pac settings
+        		var cli = app.params['chromiumPath'];
+        		cli  = '"' + cli + '"';
+        		cli += ' --proxy-pac-url="http://localhost:'+pacdata.port+'/auto.pac"';
+        		cli += ' --user-data-dir="' + __dirname + '/user-data/' + '"';
+        		cli += ' --disable-translate';
 
-			// each-other namespace
-			app.socketio.of('/eachother').on('connection', function(socket){
-				socket.on('ping', function(data, fn){
-					console.log('data:'+data.data);
-					socket.emit('pong', {data: 'pong '+data.data});
-					if (fn) fn({data: 'pong '+data.data});
-				});	
-			});
-
-			// launching chrome browser with pac settings
-			var cli = app.params['chromiumPath'];
-			cli  = '"' + cli + '"';
-			cli += ' --proxy-pac-url="http://localhost:51686/auto.pac"';
-			cli += ' --user-data-dir="' + __dirname + '/user-data/' + '"';
-			cli += ' --disable-translate';
-
-			console.log("cli: "+cli);
-			child = exec(cli);
-			child.on('exit', function(code){
-				console.log('child browser exited '+code);
-			});
-		});
+        		console.log("cli: "+cli);
+        		child = exec(cli);
+        		child.on('exit', function(code){
+        			console.log('child browser exited '+code);
+        			// exit main program
+        			process.exit(code);
+        		});
+        	});
+        }).fail(function(err){
+        	console.log('get free port failed '+err);
+        });
 	}).fail(function(err){
 		console.log('get free port failed '+err);
 	});
